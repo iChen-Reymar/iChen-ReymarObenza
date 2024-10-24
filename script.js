c97600f440ccbe11ef000159d70ed39b9d3dad90
iChen-Reymar <reymarobenza.202300232@gmail.com>
2024-10-24 15:14:35 +0800

    commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+// Initialize EmailJS with your user ID
+(function(){
+    emailjs.init('nAK-EQAw-gcLlnOnG');
+})();
+
+
+// Function to send email using EmailJS
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Send the email using the form data
+    console.log(this)
+    emailjs.sendForm('service_sjz0y8f', 'template_zsqbzks', this)
+        .then(function() {
+            alert('Message Sent Successfully!');
+        }, function(error) {
+            alert('Failed to send the message: ' + JSON.stringify(error));
+        });
+});
